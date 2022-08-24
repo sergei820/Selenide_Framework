32821978418cddc972f5f4da2bea95f95bddf0ea
Sergei Kamendrovskii <Sergei.Kamendrovskii@externals.adidas.com>
2022-08-24 13:41:04 +0400

    Selenide + Allure works

--- README.md
+++ README.md
@@ -8,7 +8,12 @@ Allure Report
 ## Running the tests under Maven
 To run tests, perform:
 
-    mvn clean verify
+    mvn clean verify -Denv=QA
+
+To check it works correctly, run this way (tests should fail because of wrong user password):
+
+    mvn clean verify -Denv=ERR
+
 
 ## Viewing the Allure Report
 settings file: src/test/resources/allure.properties
--- src/main/java/org/example/utils/Properties.java
+++ src/main/java/org/example/utils/Properties.java
@@ -4,6 +4,6 @@ import static org.example.utils.PropertiesLoader.getPropertiesInstance;
 
 public class Properties {
 
-    static final String PROPERTIES_FILE = "/QA.properties";
+    static final String PROPERTIES_FILE = String.format("/%s.properties", System.getProperty("env"));
     static final java.util.Properties PROPERTIES = getPropertiesInstance();
 }
