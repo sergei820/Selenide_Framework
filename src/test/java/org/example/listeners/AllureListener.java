8e0b21b1c9cef789d76fe9f37b9b1c75c1d72445
Sergei Kamendrovskii <Sergei.Kamendrovskii@externals.adidas.com>
2022-08-27 20:43:52 +0400

    Allure Report enhancement

---
+++ src/test/java/org/example/listeners/AllureListener.java
@@ -0,0 +1,52 @@
+package org.example.listeners;
+
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class AllureListener implements ITestListener {
+
+    @Override
+    public void onStart(ITestContext context) {
+        System.out.println("On start method " + context.getName());
+        context.setAttribute("WebDriver", WebDriverRunner.getWebDriver());
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        System.out.println("Test failed: " + getTestMethodName(result));
+        saveScreenshot(WebDriverRunner.getWebDriver());
+        saveTextLog(getTestMethodName(result) + " failed. Screenshot taken.");
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        System.out.println("Test passed: " + getTestMethodName(result));
+        saveScreenshot(WebDriverRunner.getWebDriver());
+        saveTextLog(getTestMethodName(result) + " passed. Screenshot taken.");
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        System.out.println("Test skipped: " + getTestMethodName(result));
+    }
+
+    public static String getTestMethodName(ITestResult iTestResult) {
+        return iTestResult.getMethod().getConstructorOrMethod().getName();
+    }
+
+    @Attachment//(value = "Page screenshot", type = "image/png")
+    public byte[] saveScreenshot(WebDriver driver) {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    }
+
+    @Attachment(value = "{0}", type = "text/plain")
+    public static String saveTextLog(String message) {
+        return message;
+    }
+}
--- src/test/java/org/example/tests/BaseTest.java
+++ src/test/java/org/example/tests/BaseTest.java
@@ -3,14 +3,17 @@ package org.example.tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import org.example.enums.BrowserEnum;
+import org.example.listeners.AllureListener;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import com.codeborne.selenide.testng.ScreenShooter;
+import org.testng.annotations.Listeners;
 
 import static org.example.browserfactory.BrowserFactory.getLocalBrowser;
 import static org.example.browserfactory.BrowserFactory.getSaucelabsDriver;
 
-
+@Listeners({AllureListener.class})
 public class BaseTest {
 
     @BeforeSuite
